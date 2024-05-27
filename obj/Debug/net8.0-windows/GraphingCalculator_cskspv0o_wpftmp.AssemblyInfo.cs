b898ca051e84889cb463f2409530f797b800fea7
LandonThomp <134389324+LandonThomp@users.noreply.github.com>
2024-05-27 16:01:09 -0700

    Format Changes in MainWindow.xaml

--- obj/Debug/net8.0-windows/GraphingCalculator_cskspv0o_wpftmp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/GraphingCalculator_cskspv0o_wpftmp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphingCalculator")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f352b516c23850620ca134b2bf1a9677cf36e5cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphingCalculator")]
